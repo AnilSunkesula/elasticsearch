d513098cb36b2f5dcabcdfbb00497b06c10c6f78
AnilSunkesula <32665062+AnilSunkesula@users.noreply.github.com>
2020-08-25 00:26:12 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt-get update -y
 sudo apt-get install elasticsearch
-systemctl start elasticsearch.service
+sudo systemctl start elasticsearch.service
 exit 0
