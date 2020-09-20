eed4ebe469a7ef04451c7b87455d881b2ef06191
AnilSunkesula <32665062+AnilSunkesula@users.noreply.github.com>
2020-09-20 17:02:28 +0530

    Update datadiskmount.sh

--- datadiskmount.sh
+++ datadiskmount.sh
@@ -7,11 +7,11 @@ sudo parted /dev/sdc --script mklabel gpt mkpart xfspart xfs 0% 100%
 sudo mkfs.xfs /dev/sdc1
 sudo partprobe /dev/sdc1
 #Mount the disk
-sudo mkdir /datadisks/disk1
-sudo mount /dev/sdc1 /datadisks/disk1
+sudo mkdir /datadisk1
+sudo mount /dev/sdc1 /datadisk1
 sudo blkid
 #update fstab file
 vim /etc/fstab
-UUID=33333333-3b3b-3c3c-3d3d-3e3e3e3e3e3e   /datadisks/disk1   xfs   defaults,nofail   1   2
+UUID=33333333-3b3b-3c3c-3d3d-3e3e3e3e3e3e   /datadisk1   xfs   defaults,nofail   1   2
 #verify the disk
 lsblk -o NAME,HCTL,SIZE,MOUNTPOINT | grep -i "sd"
