ac1891380805617cea3cebbfd07fe832da31673b
AnilSunkesula <32665062+AnilSunkesula@users.noreply.github.com>
2020-10-03 21:18:32 +0400

    Update rpm792.sh

--- rpm792.sh
+++ rpm792.sh
@@ -1,3 +1,13 @@
+#!/bin/sh
+
+# ====================Install Java 1.8.0_262===================="
+
+sudo yum install java-1.8.0-openjdk -y
+sudo yum install java-1.8.0-openjdk-devel -y
+
+
+# ====================Install Elasticsearch===================="
+
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.9.2-x86_64.rpm
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.9.2-x86_64.rpm.sha512
 shasum -a 512 -c elasticsearch-7.9.2-x86_64.rpm.sha512 
