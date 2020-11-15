1c5a2ab3689c8c476be2061f0ec2ad7781035602
AnilSunkesula <32665062+AnilSunkesula@users.noreply.github.com>
2020-11-15 17:56:26 +0400

    Update datadiskmount.sh

--- datadiskmount.sh
+++ datadiskmount.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
+PARTED=$1
+
 #Lists the disks attached to VM
-lsblk -o NAME,HCTL,SIZE,MOUNTPOINT | grep -i "sd"
+lsblk >> /tmp/datadisklog.txt
+
 #Partition and format the disk
-sudo parted /dev/sdc --script mklabel gpt mkpart xfspart xfs 0% 100%
-sudo mkfs.xfs /dev/sdc1
-sudo partprobe /dev/sdc1
+sudo parted /dev/${PARTED} --script mklabel gpt mkpart xfspart xfs 0% 100%
+sudo mkfs.xfs /dev/${PARTED}1
+sudo partprobe /dev/${PARTED}1
+
 #Mount the disk
 sudo mkdir /datadisk1
-sudo mount /dev/sdc1 /datadisk1
-sudo blkid
+sudo mount /dev/${PARTED}1 /datadisk1
+
 #update fstab file
-vim /etc/fstab
-UUID=33333333-3b3b-3c3c-3d3d-3e3e3e3e3e3e   /datadisk1   xfs   defaults,nofail   1   2
-#verify the disk
-lsblk -o NAME,HCTL,SIZE,MOUNTPOINT | grep -i "sd"
+echo "UUID=(blkid -o value -s UUID /dev/${PARTED}1   /datadisk1   xfs   defaults,nofail   1   2" >> /etc/fstab
+
+echo "datadisk is mounted successfully" >> /tmp/datadisklog.txt
+lsblk >> /tmp/datadisklog.txt
