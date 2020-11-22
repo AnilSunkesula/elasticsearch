6cbe0b8b695abfd288022ec1e2f49afc22abc749
AnilSunkesula <32665062+AnilSunkesula@users.noreply.github.com>
2020-11-22 17:18:48 +0400

    Update rollingrestart.sh

--- rollingrestart.sh
+++ rollingrestart.sh
@@ -1,5 +1,4 @@
-IP=$1
-curl -X PUT "10.160.89.18:9200/_cluster/settings?pretty" -H 'Content-Type: application/json' -d'
+curl -X PUT "10.160.89.17:9200/_cluster/settings?pretty" -H 'Content-Type: application/json' -d'
 {
   "persistent": {
     "cluster.routing.allocation.enable": "primaries"
@@ -7,15 +6,15 @@ curl -X PUT "10.160.89.18:9200/_cluster/settings?pretty" -H 'Content-Type: appli
 }
 '
 
-curl -X POST "10.160.89.18:9200/_flush/synced?pretty"
+curl -X POST "10.160.89.17:9200/_flush/synced?pretty"
 
-curl -X POST "10.160.89.18:9200/_ml/set_upgrade_mode?enabled=true&pretty"
+curl -X POST "10.160.89.17:9200/_ml/set_upgrade_mode?enabled=true&pretty"
 
 sudo systemctl stop elasticsearch.service
 
-curl -X GET "10.160.89.18:9200/_cat/nodes?pretty"
+curl -X GET "10.160.89.17:9200/_cat/nodes?pretty"
 
-curl -X PUT "10.160.89.18:9200/_cluster/settings?pretty" -H 'Content-Type: application/json' -d'
+curl -X PUT "10.160.89.17:9200/_cluster/settings?pretty" -H 'Content-Type: application/json' -d'
 {
   "persistent": {
     "cluster.routing.allocation.enable": null
@@ -23,4 +22,4 @@ curl -X PUT "10.160.89.18:9200/_cluster/settings?pretty" -H 'Content-Type: appli
 }
 '
 
-curl -X POST "10.160.89.18:9200/_ml/set_upgrade_mode?enabled=false&pretty"
+curl -X POST "10.160.89.17:9200/_ml/set_upgrade_mode?enabled=false&pretty"
