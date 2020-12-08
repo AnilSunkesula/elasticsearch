9f7eff43b6a8e9bf0cf4f712cef1c0b9029320b5
AnilSunkesula <32665062+AnilSunkesula@users.noreply.github.com>
2020-12-08 12:21:24 +0400

    Update esrpm632.sh

--- esrpm632.sh
+++ esrpm632.sh
@@ -3,7 +3,7 @@
 # ====================Install Java 1.8.0_262===================="
 
 sudo yum install java-1.8.0-openjdk -y
-sudo yum install java-1.8.0-openjdk-devel -y
+# sudo yum install java-1.8.0-openjdk-devel -y
 
 
 # ====================Install Elasticsearch===================="
@@ -11,6 +11,7 @@ sudo yum install java-1.8.0-openjdk-devel -y
 rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.3.2.rpm
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.3.2.rpm.sha512
+yum install -y perl-Digest-SHA
 shasum -a 512 -c elasticsearch-6.3.2.rpm.sha512
 sudo rpm --install elasticsearch-6.3.2.rpm
 sudo systemctl start elasticsearch.service
