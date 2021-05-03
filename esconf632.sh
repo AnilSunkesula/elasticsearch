59e11e336384572565ef60a95505869f17697393
AnilSunkesula <32665062+AnilSunkesula@users.noreply.github.com>
2021-05-03 16:36:24 +0400

    Update esconf632.sh

--- esconf632.sh
+++ esconf632.sh
@@ -18,13 +18,13 @@ echo "discovery.zen.minimum_master_nodes: 2" | sudo tee -a /etc/elasticsearch/el
 echo "bootstrap.system_call_filter: false" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
 echo "node.data: true" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
 echo "node.master: true" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
-sudo sed -i "s/-Xms1g/-Xms8g/g" /etc/elasticsearch/jvm.options
-sudo sed -i "s/-Xmx1g/-Xmx8g/g" /etc/elasticsearch/jvm.options
+sudo sed -i "s/-Xms1g/-Xms4g/g" /etc/elasticsearch/jvm.options
+sudo sed -i "s/-Xmx1g/-Xmx4g/g" /etc/elasticsearch/jvm.options
 echo "discovery.zen.ping.unicast.hosts:" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
 echo "        - $NODE1IP" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
 echo "        - $NODE2IP" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
 echo "        - $NODE3IP" | sudo tee -a /etc/elasticsearch/elasticsearch.yml 
-sudo sed -i "s/path.data: \/var\/lib\/elasticsearch/path.data: \/datadisk1\/elasticsearch/g" /etc/elasticsearch/elasticsearch.yml
+sudo sed -i "s/path.data: \/var\/lib\/elasticsearch/path.data: \/datadisc1\/elasticsearch/g" /etc/elasticsearch/elasticsearch.yml
 sudo mkdir /etc/systemd/system/elasticsearch.service.d
 echo "[Service]" >> /etc/systemd/system/elasticsearch.service.d/override.conf
 echo "LimitMEMLOCK=infinity" >> /etc/systemd/system/elasticsearch.service.d/override.conf
